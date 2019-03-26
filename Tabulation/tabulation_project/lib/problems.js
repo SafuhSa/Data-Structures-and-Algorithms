132a6982c436307b6c8498a43fea4e4a09fae820
Safuh <Safoh95@windowslive.com>
2019-03-26 11:17:26 -0700

    new code

--- Tabulation/tabulation_project/lib/problems.js
+++ Tabulation/tabulation_project/lib/problems.js
@@ -10,6 +10,21 @@
 // stepper([3, 1, 0, 5, 10]);           // => true, because we can step through elements 3 -> 5 -> 10
 // stepper([3, 4, 1, 0, 10]);           // => true, because we can step through elements 3 -> 4 -> 10
 // stepper([2, 3, 1, 1, 0, 4, 7, 8])    // => false, there is no way to step to the end
+// function stepper(nums, startIdx=0,  memo={}) {
+//     memo[0] = true;
+//     let len = nums.length;
+//     if(len in memo) return true;
+//     let el = nums[startIdx];
+
+//     for (let i = 1; i <= el; i++) {
+//         memo[startIdx + i] = true;
+//         if (startIdx + i >= len) return true;
+
+//         stepper(nums, startIdx + i, memo)
+//     }
+    
+//     return memo[len] ? true : false
+// }
 function stepper(nums) {
     let arr = new Array(nums.length).fill(false);
     arr[0] = true;
@@ -24,7 +39,7 @@ function stepper(nums) {
         }
         i++;
     }
-    
+
     return arr[nums.length - 1]
 }
 
@@ -41,6 +56,15 @@ function stepper(nums) {
 // maxNonAdjacentSum([4,2,1,6])         // => 10, because 4 + 6 
 function maxNonAdjacentSum(nums) {
 
+    let prev = 0;
+    let beforePrev = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let el = nums[i];
+        let temp = prev;
+        prev = Math.max(beforePrev + el, prev);
+        beforePrev = temp;
+    }
+    return prev;
 }
 
 
@@ -57,7 +81,17 @@ function maxNonAdjacentSum(nums) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount) {
+    let arr = new Array(amount + 1).fill(amount*3);
+    arr[0] = 0
 
+    for(let i = 1; i <= amount; i++) {
+        for(coin of coins) {
+            if(i < coin) continue;
+            arr[i] = Math.min(arr[i], arr[i-coin]+1)
+        }
+    }
+
+    return arr[amount] >= amount*13 ? 0 : arr[amount];
 }
 
 
