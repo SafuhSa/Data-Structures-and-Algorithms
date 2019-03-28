5e05a46ec12baf52cf772da4f8e6bdfe7b30c7dc
Safuh <Safoh95@windowslive.com>
2019-03-28 13:07:26 -0700

    sorting

--- sorting algo/selection_sort_project/lib/selection_sort.js
+++ sorting algo/selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,21 @@
 function swap(arr, index1, index2) {
-
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
 }
 
 function selectionSort(arr) {
+    for(let i = 0; i < arr.length-1; i++) {
+        let minIdx = i;
 
+
+        for(let j = i+1; j < arr.length; j++) {
+            if(arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        swap(arr, i, minIdx)
+    }
 }
 
 module.exports = {
