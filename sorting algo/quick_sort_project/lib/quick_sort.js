60c63c79533207748391819b18f7c76acf78bf02
Safuh <Safoh95@windowslive.com>
2019-03-29 12:27:55 -0700

    quicksort solution

--- sorting algo/quick_sort_project/lib/quick_sort.js
+++ sorting algo/quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,13 @@
 function quickSort(array) {
+    if(array.length <= 1) return array;
 
+    let rand = Math.floor(Math.random() * array.length);
+    let pivt = array.splice(rand, 1)[0];
+
+    let left = array.filter(el => (el < pivt));
+    let right = array.filter(el => (el >= pivt));
+
+    return [...quickSort(left), pivt, ...quickSort(right)];
 }
 
 
