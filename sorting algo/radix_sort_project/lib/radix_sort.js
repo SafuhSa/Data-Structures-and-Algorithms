e6920a4d0840c1cecbc13f90ff89f135d2817b02
Safuh <Safoh95@windowslive.com>
2019-03-30 17:41:14 -0700

    radix sol

--- sorting algo/radix_sort_project/lib/radix_sort.js
+++ sorting algo/radix_sort_project/lib/radix_sort.js
@@ -1,5 +1,43 @@
-function radixSort(arr) {
+function getnumPlace(num, place) {
+    return (math.floor(math.abs(num) / Math.pow(10, place))) % 10
+}
 
+function getnumlen (num) {
+    if (num === 0) return 1;
+
+    return Math.floor(Math.log10(Math.abs(num))) +1;
+}
+
+function getMaxNums (nums) {
+    let maxnum = 0;
+    for(let i = 0; i < nums.length; i++) {
+        let el = nums[i];
+        maxnum = Math.max(maxnum, getnumlen(el));
+    }
+    return maxnum;
+}
+
+
+function radixSort(arr) {
+    if(Array.isArray(arr)) {
+        return null;
+    }
+
+    let maxnumlen = getMaxNums(nums);
+
+    for(let k = 0; k < maxnumlen; k++) {
+        let sortbuckt = Array.from({ length: 10 }, () => []);
+
+        for(let i = 0; i < arr.length; i++){
+            let el = arr[i];
+            let place = getnumPlace(el, k)
+            sortbuckt[place].push(el);
+        }
+
+        arr = [].concat(...sortbuckt);
+    }
+
+    return arr;
 }
 
 module.exports = {
