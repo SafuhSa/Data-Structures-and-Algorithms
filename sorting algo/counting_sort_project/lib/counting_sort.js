6cdedb9fb36eb7db19c50e66cfca494217818962
Safuh <Safoh95@windowslive.com>
2019-03-31 12:09:06 -0700

    code

--- sorting algo/counting_sort_project/lib/counting_sort.js
+++ sorting algo/counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,20 @@
 function countingSort(arr, max) {
+    if(arr.length < 2) return arr;
 
+    let counter = new Array(max+1).fill(0);
+
+    for(let i = 0; i < arr.length; i++) {
+        let el = arr[i];
+        counter[el]++;
+    }
+    let sorted = [];
+    for(let j = 0; j < counter.length; j++) {
+        while(counter[j] > 0) {
+            sorted.push(j);
+            counter[j] -= 1;
+        }
+    }
+    return sorted;
 }
 
 
