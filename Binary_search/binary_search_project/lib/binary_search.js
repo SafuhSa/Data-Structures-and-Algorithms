aba0038b14a611c8bda220ccc7745ab75ca9380c
Safuh <Safoh95@windowslive.com>
2019-04-01 13:59:42 -0700

    in place binary search idx

--- Binary_search/binary_search_project/lib/binary_search.js
+++ Binary_search/binary_search_project/lib/binary_search.js
@@ -14,30 +14,42 @@ function binarySearch(array, target) {
         return binarySearch(left, target);
     }
 }
-3
-[1, 2, 3, 4, 5, 6, 7]
-function binarySearchIndex(array, target) {
-    if(!array.length) return -1;
 
-    let midIdx = Math.floor(array.length / 2);
-    let midel = array[midIdx];
-    let left = array.slice(0, midIdx);
-    let right = array.slice(midIdx+1);
+function binarySearchIndex(array, target, lo=0, hi=array.length-1) {
+    if(lo === hi) {
+        return -1;
+    }
 
-    if(midel === target) {
-        return midIdx;
-    }else if(midel < target) {
-        let ans =  binarySearchIndex(right, target) 
-        if(ans > -1) {
-            return ans + midIdx + 1;
-        } else {
-            return ans;
-        }
+    let midIdx = Math.floor( (lo + hi) / 2);
+
+    if(target < array[midIdx]) {
+        return binarySearchIndex(array, target, lo, midIdx);
+    }else if(target > array[midIdx]) {
+        return binarySearchIndex(array, target, midIdx + 1, hi);
     }else {
-        return binarySearchIndex(left, target);
+        return midIdx;
     }
 }
 
+// if(!array.length) return -1;
+
+// let midIdx = Math.floor(array.length / 2);
+// let midel = array[midIdx];
+// let left = array.slice(0, midIdx);
+// let right = array.slice(midIdx+1);
+
+// if(midel === target) {
+//     return midIdx;
+// }else if(midel < target) {
+//     let ans =  binarySearchIndex(right, target) 
+//     if(ans > -1) {
+//         return ans + midIdx + 1;
+//     } else {
+//         return ans;
+//     }
+// }else {
+//     return binarySearchIndex(left, target);
+// }
 
 module.exports = {
     binarySearch,
