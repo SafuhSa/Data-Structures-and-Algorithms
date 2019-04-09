dd9676bb13468956ace2fecbe58cae766ca5e677
Safuh <Safoh95@windowslive.com>
2019-04-09 16:03:17 -0700

    stack sol

--- stacksAndQueues/stack_project/lib/stack.js
+++ stacksAndQueues/stack_project/lib/stack.js
@@ -21,11 +21,43 @@
 // -----------
 
 class Node {
-
+  constructor(val = null, next=null) {
+    this.value = val;
+    this.next = next;
+  }
 }
 
 class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val)
+    if(!this.bottom) {
+      this.bottom = newNode;
+    }
+    newNode.next = this.top;
+    this.top = newNode;
+    this.length++;
+    return this.length;
+  }
 
+  pop() {
+    if (!this.length) return null;
+    if(this.top === this.bottom) {
+      this.bottom = null;
+    }
+    
+    let removedtop = this.top;
+    this.top = this.top.next;
+    this.length -=1;
+    return removedtop.value;
+  }
+  size() {
+    return this.length;
+  }
 }
 
 exports.Node = Node;
