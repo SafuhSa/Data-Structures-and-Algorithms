c65f490f96fec269528c90b9545214ff7e6c0ec5
Safuh <Safoh95@windowslive.com>
2019-05-01 12:50:27 -0700

    treeSum

--- TreeOrder/depth_breadth_first_project/lib/tree_height.js
+++ TreeOrder/depth_breadth_first_project/lib/tree_height.js
@@ -1,12 +1,22 @@
 function treeHeight(root) {
     let height = -1;
-
     let stack = [];
+
     if(root) stack.push(root);
 
     while(stack.length) {
+        let arr = [];
 
+        for(let i = 0; i < stack.length; i++) {
+            let node = stack[i];
+
+          if(node.left)  arr.push(node.left);
+          if(node.right) arr.push(node.right);
+        }
+        stack = arr;
+        height++;
     }
+    return height;
 }
 
 
