998f8e0c6397bf6bf46c2f1f6009ced12aab83a4
Safuh <Safoh95@windowslive.com>
2019-05-04 14:08:03 -0700

    maxValue

--- Graph/graph_project/lib/max_value.js
+++ Graph/graph_project/lib/max_value.js
@@ -1,5 +1,18 @@
 function maxValue(node, visited=new Set()) {
-    
+    if(visited.has(node)) return;
+
+    visited.add(node);
+
+    let max = node.val;
+
+    node.neighbors.forEach(node1 => {
+        let recMax = maxValue(node1, visited);
+        if (max < recMax) {
+            max = recMax;
+       }
+    });
+
+    return max;
 }
 
 module.exports = {
