2b6904625212d136a2370da3c4eaa52e996635a7
Safuh <Safoh95@windowslive.com>
2019-05-05 14:58:17 -0700

    numRegions

--- Graph/graph_project/lib/num_regions.js
+++ Graph/graph_project/lib/num_regions.js
@@ -1,4 +1,13 @@
 function numRegions(graph) {
+    let visted = new Set();
+    let counter = 0;
+     
+    for(let key in graph) {
+        let 
+        if(!visted.has(key)) {
+            counter++;
+        }
+    }
 
 }
 
