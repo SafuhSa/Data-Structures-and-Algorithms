7534f1b6976777c1dd4bd97c165b26ba2387796e
Safuh <Safoh95@windowslive.com>
2019-05-11 11:09:22 -0700

    tires

--- Tries/trie_project/lib/trie.js
+++ Tries/trie_project/lib/trie.js
@@ -11,7 +11,6 @@ class Trie {
    }
 
     insertRecur(word, root=this.root) {
-
         let letter = word[0];
         if(!root.children[letter]) {
             root.children[letter] = new Node();
@@ -41,13 +40,11 @@ class Trie {
         if(word.length === 0) return root.isTerminal;
         let letter = word[0];
         if(!root.children[letter]) return false;
-        
 
         return this.searchRecur(word.slice(1), root.children[letter])
     }
 
     searchIter(word) {
-
         let root = this.root;
 
         for(let letter of word) {
@@ -55,7 +52,6 @@ class Trie {
 
             root = root.children[letter];
         }
-
 
         return root.isTerminal;
     }
@@ -65,8 +61,8 @@ class Trie {
         let children = root.children
         for(let child in children) {
             if(prefix !== '' && prefix[0] !== child) continue;
-            if (children[child].isTerminal && prefix.length < 2) result.push(child)
-
+            if (children[child].isTerminal && prefix.length < 2) result.push(child);
+            
             let words = this.wordsWithPrefix(prefix.slice(1), children[child])
             result.push(...words.map(word => child+word))
         }
